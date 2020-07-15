a2c9759b8646035d7f490ba870f44aa98065cccd
Red-Alert <35522795+StevenXu96@users.noreply.github.com>
2020-07-15 18:55:25 -0400

    finished some axios get request with console log

--- src/search.js
+++ src/search.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import axios from 'axios';
 
 class Search extends React.Component{
     constructor(props){
@@ -7,6 +8,7 @@ class Search extends React.Component{
             name: "",
         }
         this.handleChange = this.handleChange.bind(this);
+        this.handleClick = this.handleClick.bind(this);
     }
 
     handleChange(event){
@@ -16,14 +18,28 @@ class Search extends React.Component{
         })
     }
 
+    handleClick(){
+        const info = this.state.name;
+        axios.get('https://www.balldontlie.io/api/v1/players?search=' + info + '&page=2')
+            .then(response => {
+                if (response.data.data.length > 0){
+                    const list_of_players = [];
+                    response.data.data.map(player => 
+                        list_of_players.push(player.first_name)
+                    )
+                    console.log(list_of_players);
+                }
+            })
+    }
+
     render(){
         return(
             <div className="search_bar_container">
                 <input className="name" type="text" placeholder="Player Name" onChange={this.handleChange} value={this.state.name} />
+                <button className="search" onClick={this.handleClick}>Search</button>
             </div>
         )
     }
-
 }
 
 export default Search
