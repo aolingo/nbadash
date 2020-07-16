3b07610499160341c696e353c5add102c47951c4
Red-Alert <35522795+StevenXu96@users.noreply.github.com>
2020-07-15 20:21:19 -0400

    list of players

--- src/search.js
+++ src/search.js
@@ -6,6 +6,7 @@ class Search extends React.Component{
         super(props);
         this.state = {
             name: "",
+            players: []
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleClick = this.handleClick.bind(this);
@@ -20,14 +21,16 @@ class Search extends React.Component{
 
     handleClick(){
         const info = this.state.name;
-        axios.get('https://www.balldontlie.io/api/v1/players?search=' + info + '&page=2')
+        axios.get('https://www.balldontlie.io/api/v1/players?search=' + info + '&page=1')
             .then(response => {
                 if (response.data.data.length > 0){
                     const list_of_players = [];
                     response.data.data.map(player => 
                         list_of_players.push(player.first_name)
                     )
-                    console.log(list_of_players);
+                    this.setState({
+                        players: list_of_players
+                    })
                 }
                 else {
                     alert("there is no player named " + info + " !");
@@ -40,6 +43,13 @@ class Search extends React.Component{
             <div className="search_bar_container">
                 <input className="name" type="text" placeholder="Player Name" onChange={this.handleChange} value={this.state.name} />
                 <button className="search" onClick={this.handleClick}>Search</button>
+                <ul className="list_of_players">
+                    {this.state.players.map((value, idx) => (
+                        <li key={idx} className="individual_player">
+                            {value}
+                        </li>
+                    ))}
+                </ul>
             </div>
         )
     }
